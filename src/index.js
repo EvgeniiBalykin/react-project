4dbd5d2b88ae5aaa297df3a2cec8a5fd70bb2cc5
EvgeniiBalykin <sell280596@gmail.com>
2022-04-17 15:01:09 +0200

    complete task 12 with props

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Text from './Text';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Text />
+  <div className='nav-text'>
+    <Text text ="Regular text"/>
+    <Text text ="Huge header" fontSize={70}/>
+    <Text text ="Danger notification" textColor={'red'}/>
+    <Text text ="Underlined text" textDecoration={true}/>
+  </div>
   </React.StrictMode>
 );
 
