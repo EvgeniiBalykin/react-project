2d46f6b87836a170c166b8e31e84de9911dd75ac
EvgeniiBalykin <sell280596@gmail.com>
2022-04-20 14:50:30 +0200

    complete the task 7 State Cyclic button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Counter from './Task';
+import MuseButton from './Task';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter/>
+    <MuseButton/>
   </React.StrictMode>
 );
 
