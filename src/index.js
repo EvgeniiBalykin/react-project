79e5c58d83706c86e02dd1bc9c9f53f004ec2e0d
EvgeniiBalykin <sell280596@gmail.com>
2022-04-22 11:59:50 +0200

    complete task 8 State Cursive text

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Counter from './Task';
+import ItalicText from './Italic';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter/>
+    <ItalicText text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} title='Cursive' color={'black'}/>
   </React.StrictMode>
 );
 
