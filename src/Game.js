e897d079b383162b134025fb6ec2fb56e6f1290e
Евгений Балыкин <sell280596@gmail.com>
2022-05-03 14:33:14 +0200

    debug code

--- src/Game.js
+++ src/Game.js
@@ -6,21 +6,21 @@ function Game() {
   const [, setCount] = useState(0);
 
   const getRandomInt = (min, max) => {
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
   }
 
   function getRandomColor() {
-    let x= getRandomInt(0, 255);
-    let y = getRandomInt(0, 255);
-    let z = getRandomInt(0, 255);
+    const x= getRandomInt(0, 255);
+    const y = getRandomInt(0, 255);
+    const z = getRandomInt(0, 255);
     return `rgb(${x},${y},${z})`
   }
 
   const top = getRandomInt(0, 900) + 'px'
   const left = getRandomInt(0, 900) + 'px'
-
   const color = getRandomColor()
+  
   const styles = {
     background: color,
     top: top,
