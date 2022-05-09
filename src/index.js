baf757b241f82f780c980d98b09a68a023517957
Евгений Балыкин <sell280596@gmail.com>
2022-05-09 13:25:03 +0200

    complete task_17

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Counter from './Task';
+import SayHi from './SayHi';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter/>
+    <SayHi/>
   </React.StrictMode>
 );
 
